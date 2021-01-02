e0382cd95bbdb2377d2a7892bad5cce1c3732688
didiermunezer38 <didiermunezer38@gmail.com>
2021-01-02 16:03:30 +0200

    adding mongoose module from nest/mongoose

--- nest-graphql/src/app.module.ts
+++ nest-graphql/src/app.module.ts
@@ -3,12 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemsModule } from './items/items.module';
 import { GraphQLModule } from '@nestjs/graphql';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [ItemsModule,
     GraphQLModule.forRoot({
       autoSchemaFile: 'schema.gql',
-     })],
+     }),
+     MongooseModule.forRoot('mongodb://localhost/nestgraphql')
+    ],
   controllers: [AppController],
   providers: [AppService],
 })
