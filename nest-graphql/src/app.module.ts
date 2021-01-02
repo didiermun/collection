6433c49ea59fc9e7fe55f436603de30c09b7546c
didiermunezer38 <didiermunezer38@gmail.com>
2021-01-02 16:02:32 +0200

    import graphql in the app module

--- nest-graphql/src/app.module.ts
+++ nest-graphql/src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemsModule } from './items/items.module';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [ItemsModule],
+  imports: [ItemsModule,
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql',
+     })],
   controllers: [AppController],
   providers: [AppService],
 })
