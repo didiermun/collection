94d0a2b95be177804bbbf037cc423fd4bbb56468
didiermunezer38 <didiermunezer38@gmail.com>
2021-01-02 12:23:45 +0200

    adding swagger even not working

--- fastify-api/src/index.js
+++ fastify-api/src/index.js
@@ -2,11 +2,21 @@
 const fastify = require('fastify')({
     logger: true
   })
+  const routes = require('./routes')
+
+  // Import Swagger Options
+const swagger = require('./config/swagger')
+
+// Register Swagger
+fastify.register(require('fastify-swagger'), swagger.options)
   
   // Declare a route
   fastify.get('/', async (request, reply) => {
     return { hello: 'world' }
   })
+  routes.forEach((route, index) => {
+    fastify.route(route)
+   })
 
   // Require external modules
 const mongoose = require('mongoose')
@@ -14,6 +24,7 @@ const mongoose = require('mongoose')
 const start = async () => {
     try {
       await fastify.listen(3000)
+      fastify.swagger()
       fastify.log.info(`server listening on ${fastify.server.address().port}`)
     } catch (err) {
       fastify.log.error(err)
--- fastify-api/src/routes/index.js
+++ fastify-api/src/routes/index.js
@@ -16,7 +16,6 @@ const routes = [
     method: 'POST',
     url: '/api/cars',
     handler: carController.addCar,
-    schema: documentation.addCarSchema
   },
   {
     method: 'PUT',
