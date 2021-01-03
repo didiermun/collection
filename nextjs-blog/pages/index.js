dacc1b1d0b7047cc02a09a424e439b15a6db88e7
didiermunezer38 <didiermunezer38@gmail.com>
2021-01-03 10:35:40 +0200

    starter to css config

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -12,7 +13,8 @@ export default function Home() {
         <p>I am a software engineer looking forward</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          <Link href="posts/first-post">My first post)</Link>
+          
         </p>
       </section>
     </Layout>
