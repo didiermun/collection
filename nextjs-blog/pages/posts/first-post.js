3f11f40576c58a531076de40da307c294a9b4a52
didiermunezer38 <didiermunezer38@gmail.com>
2021-01-03 09:58:46 +0200

    adding title to page

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,3 +1,19 @@
+
+import Head from 'next/head'
+import Link from 'next/link'
+
 export default function FirstPost() {
-    return <h1>First Post</h1>
+    return (
+      <>
+        <Head>
+          <title>First Post</title>
+        </Head>
+        <h1>First Post</h1>
+        <h2>
+          <Link href="/">
+            <a>Back to home</a>
+          </Link>
+        </h2>
+      </>
+    )
   }
